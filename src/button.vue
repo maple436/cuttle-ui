82d1b946397f09f21504272f8f8b311e4b4bc84a
maple <2313694690@qq.com>
2020-08-18 20:42:37 +0800

    填写 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,17 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default {}
+  export default {
+    props:['icon','iconPosition']
+  }
 </script>
 <style lang="scss">
   .g-button {
@@ -12,14 +21,50 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
     }
+
+    > .content {
+      order: 2;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
+
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
+    }
+  }
+</style>
+<style>
+  .icon {
+    width: 1em; height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
   }
 </style>
