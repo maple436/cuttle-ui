69ce42573e12604b7221977cb70f6905be597477
maple <2313694690@qq.com>
2020-08-24 21:04:34 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-    <div style="border: 1px solid green; height: 100px;">
+    <div class="col" :class="colClass" :style="colStyle">
       <slot></slot>
     </div>
-  </div>
 </template>
 <script>
   export default {
@@ -20,6 +17,21 @@
       },
       offset:{
         type:[Number,String]
+      }
+    },
+    computed:{
+      colClass(){
+        let {span,offset}=this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle(){
+        return {
+          paddingLeft:this.gutter/2+'px',
+          paddingRight:this.gutter/2+'px',
+        }
       }
     }
   }
