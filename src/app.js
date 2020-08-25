a56e6034b142c449a1b458865d69f09d7749f9e6
maple <2313694690@qq.com>
2020-08-25 20:46:40 +0800

    添加 toast 和 plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,8 @@ Vue.component('g-header', Header)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
 
 new Vue({
   el: '#app',
@@ -31,18 +35,12 @@ new Vue({
     loading3:false,
     message:"Hello Gulu"
   },
-  // created(){
-  //   setTimeout(()=>{
-  //     let event = new Event('change');
-  //     let inputElement = this.$el.querySelector('input')
-  //     inputElement.dispatchEvent(event)
-  //     console.log('hi')
-  //   },3000)
-  // },
-  // methods: {
-  //   inputChange(e) {
-  //     console.log(e)
-  //   }
 
+  methods: {
+    showToast(){
+      this.$toast('我是 message')
+    }
+  }
 })
+
 
