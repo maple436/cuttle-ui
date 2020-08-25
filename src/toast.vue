f4632a2461b9c0630187bf11489f06a3f406d4b6
maple <2313694690@qq.com>
2020-08-25 22:17:39 +0800

    解决 currentToast 的一个 bug

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="wrapper">
+  <div class="toast" ref="wrapper" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div>
@@ -34,11 +34,23 @@
       enableHtml:{
         type:Boolean,
         default:false,
+      },
+      position: {
+        type: String,
+        default: 'top',
+        validator (value) {
+          return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+        }
+      },
+    },
+    computed: {
+      toastClasses () {
+        return {
+          [`position-${this.position}`]: true
+        }
       }
     },
-    created(){
 
-    },
     mounted(){
       this.updateStyles()
       this.execAutoClose()
@@ -59,6 +71,7 @@
       },
       close () {
         this.$el.remove()
+        this.$emit('close')
         this.$destroy()
       },
       log () {
@@ -80,9 +93,10 @@
   $toast-bg: rgba(0, 0, 0, 0.75);
   .toast {
     font-size: $font-size; min-height: $toast-min-height; line-height: 1.8;
-    position: fixed; top: 0; left: 50%; transform: translateX(-50%); display: flex;
+    position: fixed;  display: flex;
     color: white; align-items: center; background: $toast-bg; border-radius: 4px;
     box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
+    left: 50%;
     .message {
       padding: 8px 0;
     }
@@ -94,6 +108,18 @@
       height: 100%;
       border-left: 1px solid #666;
       margin-left: 16px;
+    }
+    &.position-top{
+      top: 0;
+      transform: translateX(-50%);
+    }
+    &.position-bottom{
+      bottom: 0;
+      transform: translateX(-50%);
+    }
+    &.position-middle{
+      top: 50%;
+      transform: translate(-50%, -50%);
     }
   }
 </style>
