e5a931476d01e19a71f911b12707604083392331
maple <2313694690@qq.com>
2020-08-26 21:53:16 +0800

    添加 disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -25,7 +25,8 @@
     computed:{
       classes () {
         return {
-          active: this.active
+          active: this.active,
+          disabled:this.disabled
         }
       }
     },
@@ -35,13 +36,16 @@
       })
     },
     methods:{
-      xxx(){
+      onClick(){
+        if(this.disabled){return}
         this.eventBus.$emit('update:selected',this.name,this)
       }
     }
   }
 </script>
 <style lang="scss" scoped >
+  $blue: blue;
+  $disabled-text-color: grey;
   .tabs-item {
     flex-shrink:0;
     padding:0 1em;
@@ -49,9 +53,13 @@
     height: 100%;
     display:flex;
     align-items: center;
-
     &.active{
       font-weight:bold;
+      color: $blue;
+    }
+    &.disabled {
+      color: $disabled-text-color;
+      cursor: not-allowed;
     }
   }
 </style>
