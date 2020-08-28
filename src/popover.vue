fb86fd241604e00b03667b388b75bc2baf5aebd6
maple <2313694690@qq.com>
2020-08-28 11:41:10 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" >
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -16,11 +18,14 @@
     methods: {
       xxx () {
         this.visible = !this.visible
-        if (this.visible === true) {
+        if (this.visible === true){
           this.$nextTick(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             let eventHandler = () => {
               this.visible = false
-              console.log('document 隐藏 popover')
               document.removeEventListener('click', eventHandler)
             }
             document.addEventListener('click', eventHandler)
@@ -40,10 +45,9 @@
     position: relative;
     .content-wrapper {
       position: absolute;
-      bottom: 100%;
-      left: 0;
       border: 1px solid red;
       box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+      transform: translateY(-100%);
     }
   }
 </style>
