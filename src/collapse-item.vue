01468576fa42804c8c7b0cc21b6b84911b6e13f8
maple <2313694690@qq.com>
2020-08-29 15:49:30 +0800

    完成 collapse 组件

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
+    <div class="title" @click="toggle" :data-name="name">
       {{title}}
     </div>
-    <div class="content" v-if="open">
+    <div class="content" ref="content" v-if="open">
       <slot></slot>
     </div>
   </div>
@@ -16,13 +16,36 @@
       title: {
         type: String,
         required: true
+      },
+      name: {
+        type: String,
+        required: true
       }
     },
     data () {
       return {
-        open: false
+        open: false,
       }
-    }
+    },
+    inject: ['eventBus'],
+    mounted () {
+      this.eventBus && this.eventBus.$on('update:selected', (names) => {
+        if (names.indexOf(this.name) >= 0) {
+          this.open = true
+        } else {
+          this.open = false
+        }
+      })
+    },
+    methods: {
+      toggle () {
+        if (this.open) {
+          this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
+        } else {
+          this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
+        }
+      },
+    },
   }
 </script>
 
@@ -30,30 +53,16 @@
   $grey: #ddd;
   $border-radius: 4px;
   .collapseItem {
-    > .title {
-      border: 1px solid $grey;
-      margin-top: -1px;
-      margin-left: -1px;
-      margin-right: -1px;
-      min-height: 32px;
-      display: flex;
-      align-items: center;
-      padding: 0 8px;
+    > .title { border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px;
+      min-height: 32px; display: flex; align-items: center; padding: 0 8px;
+      background: lighten($grey, 8%);
     }
     &:first-child {
-      > .title {
-        border-top-left-radius: $border-radius;
-        border-top-right-radius: $border-radius;
-      }
+      > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
     }
     &:last-child {
-      > .title:last-child {
-        border-bottom-left-radius: $border-radius;
-        border-bottom-right-radius: $border-radius;
-      }
+      > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
     }
-    > .content {
-      padding: 8px;
-    }
+    > .content { padding: 8px; }
   }
 </style>
--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,48 @@
 </template>
 
 <script>
+  import Vue from 'vue'
   export default {
-    name: "GuluCollapse"
+    name: "GuluCollapse",
+    props: {
+      single: {
+        type: Boolean,
+        default: false
+      },
+      selected: {
+        type: Array,
+      }
+    },
+    data () {
+      return {
+        eventBus: new Vue(),
+      }
+    },
+    provide () {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted () {
+      this.eventBus.$emit('update:selected', this.selected)
+      this.eventBus.$on('update:addSelected', (name) => {
+        let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+        if (this.single) {
+          selectedCopy = [name]
+        } else {
+          selectedCopy.push(name)
+        }
+        this.eventBus.$emit('update:selected', selectedCopy)
+        this.$emit('update:selected', selectedCopy)
+      })
+      this.eventBus.$on('update:removeSelected', (name) => {
+        let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+        let index = selectedCopy.indexOf(name)
+        selectedCopy.splice(index, 1)
+        this.eventBus.$emit('update:selected', selectedCopy)
+        this.$emit('update:selected', selectedCopy)
+      })
+    }
   }
 </script>
 
